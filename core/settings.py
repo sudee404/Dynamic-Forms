7f1945fe1f3e7a513c3bc68268dbaef22e3993e9
sudee404 <sudaise18@gmail.com>
2024-11-12 07:39:40 +0300

    Update settings for production environment and add new dependencies

--- core/settings.py
+++ core/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-kmc$fdx0jm7ewbcmk6+ab(xd+c7i4*k5z80)9u@9_85=tae)w#'
+SECRET_KEY = os.getenv('SECRET_KEY', 'secret')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
